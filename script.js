0ab9e9ff9e603158867fbec5f218042d9ed05fbe
q2p <q2p@users.noreply.github.com>
2021-03-20 21:16:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // @name        Chirbit.com Chirb.it Downloader
 // @description This is a simple script that shows you a direct link to audio file that you want to download.
 // @license     Creative Commons Zero v1.0 Universal
-// @supportURL  https://github.com/q2p/Soundgasm-Link-Exporter
+// @supportURL  https://github.com/q2p/Chirbit-Link-Exporter
 // @author      q2p
 // @namespace   q2p
 // @version     0.2
